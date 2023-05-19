99fed762d8885476f3937b47180338dbd5905464
Ghaieth Dridi <ghaieth.dridi@esprit.tn>
2023-05-19 13:19:32 +0100

    add files

---
+++ src/main/java/tn/esprit/apigateway/config/SpringCloudGatewayRouting.java
@@ -0,0 +1,16 @@
+package tn.esprit.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudGatewayRouting {
+    @Bean
+    public RouteLocator configureRoute(RouteLocatorBuilder builder) {
+        return builder.routes().route("produits",
+                r->r.path("/produits/**").uri("lb://Top-i7tiram"))
+                .build();
+    }
+}
